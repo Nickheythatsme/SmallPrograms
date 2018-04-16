a97da644be5570f537e739d6fb4d6b222e8e407e
Nick <njgrout@gmail.com>
2018-04-16 16:59:55 -0700

    Working on markdown stuff

---
+++ Javascript/Markdown/index.js
@@ -0,0 +1,11 @@
+const markdown_key = {
+    "###":["<h1>","</h1>"],
+    "##":["<h2>","</h2>"],
+    "#":["<h3>","</h3>"]
+}
+
+
+function markdown() {
+    var input = document.getElementById('input');
+    var output = document.getElementById('output');
+}
