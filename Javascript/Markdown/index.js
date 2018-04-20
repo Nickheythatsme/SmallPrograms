7894460e380a1ce6c2382ae15dbb0eaa6a1fd0b0
Nick <njgrout@gmail.com>
2018-04-20 15:47:59 -0700

    working on markdown

--- Javascript/Markdown/index.js
+++ Javascript/Markdown/index.js
@@ -7,5 +7,15 @@ const markdown_key = {
 
 function markdown() {
     var input = document.getElementById('input');
-    var output = document.getElementById('output');
+    var raw_output = document.getElementById('raw_output');
+
+    raw_output.value = input.value;
+    markdown_output.innerHTML = '<h1>test</h1>';
+    addMarkup();
+}
+
+function addMarkup(text="TEsting this"){
+    var t = /[a-z]/g;
+    var match = text.search(t);
+    console.log('Matches: ' + match);
 }
