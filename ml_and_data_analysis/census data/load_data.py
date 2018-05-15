22f14c11bcc6a6bf073e5cc6f34cdfaf33d24127
Nick <njgrout@gmail.com>
2018-05-14 17:35:35 -0700

    Added lecture files from a class
    Files added from http://raoulrivas.net/

--- ml_and_data_analysis/census data/load_data.py
+++ ml_and_data_analysis/census data/load_data.py
@@ -13,7 +13,7 @@ headers = [
     "occupation",
     "relationship",
     "race",
-    "sex",
+    "Female",
     "capital-gain",
     "capital-loss",
     "hours-per-week",
@@ -33,27 +33,37 @@ def load_contents(filename):
 def clean_contents(lines):
     # Remove the first row
     lines.pop(0)
+
     # Remove any rows with no data (last row)
     for i,line in enumerate(lines):
         if len(line) == 0:
             lines.pop(i)
+
     # Change income to less than or equal to 50K (True/false)
     for line in lines:
-        if line[14] == ' <=50K.':
-            line[14] = 1
-        else:
-            line[14] = 0
+        # Change age to int
+        line[0] = int(line[0])
+
+        # Change education-num to int
+        line[4] = int(line[4])
+
+        # Change sex to Female true/false
+        line[9] = line[9] == ' Female'
+
+        # Change capital gain/loss
+        line[10] = int(line[10])
+        line[11] = int(line[11])
+
+        # Change number of hours per week to int
+        line[12] = int(line[12])
+
+        # Change income to True/false
+        line[14] = line[14] == ' <=50K.'
     return lines
 
 # Make a pandas data field from data
 def make_df(data):
     df = pd.DataFrame(data, columns=headers)
-    df = convert_type(df, "age", int)
-    return df
-
-def convert_type(df, col, func):
-    for row in df.iterrows():
-        row[1][col] = func(row[1][col])
     return df
 
 def get_data(filename):
@@ -64,6 +74,7 @@ def get_data(filename):
 adult_data = get_data('./adult.data.csv')
 adult_test = get_data('./adult.test.csv')
 
+
 if __name__ == "__main__":
-    print( "Adult data head:\n{}".format(adult_data.head()))
-    print(type(adult_data["age"][0]))
+    for i in adult_test:
+        print("Name: {}\tType: {}".format(i, type(adult_test[i][0])))
