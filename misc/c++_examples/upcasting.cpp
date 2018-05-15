75e231cf35b67a4d0dfafdb162bb65346f0defef
Nicholas Grout <njgrout@gmail.com>
2018-05-15 16:25:08 -0700

    new file:   c++_examples/upcasting.cpp

---
+++ misc/c++_examples/upcasting.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+class abase
+{
+    public:
+        abase();
+};
+
+class base : public abase
+{
+    public:
+        base() {}
+        base(const base &obj) {}
+        ~base() {}
+        virtual const char* message() const {return "this is base";}
+};
+
+class deriv : public base
+{
+    public:
+        deriv() : base() {}
+        deriv(const deriv &obj) : base(obj) {}
+        ~deriv() {}
+        const char* message() const {return "this is deriv";}
+};
+
+void test(const base &rhs)
+{
+    base *ptr;
+    cout << rhs.message() << endl;
+
+    if (const base *d = dynamic_cast<const base *>(&rhs))
+    {
+        cout << "base" << endl;
+        ptr = new base(*d);
+    }
+    if (const deriv *d = dynamic_cast<const deriv *>(&rhs))
+    {
+        cout << "deriv" << endl;
+        ptr = new deriv(*d);
+    }
+    cout << endl;
+}
+
+int main(int argc, char **argv)
+{
+    base b;
+    deriv d;
+
+    test(b);
+    test(d);
+}
+
